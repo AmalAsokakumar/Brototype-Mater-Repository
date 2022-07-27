1d24fc1d7bc00892be306bd9eb0ef0955949a103
comrider <cena3799@gmail.com>
2022-07-27 17:32:47 +0530

    new files are added

---
+++ workspaces /python/getting_started_with_python.py
@@ -0,0 +1,121 @@
+# print ("Hello, Python!")
+# print ("interpreted \nobject-oriented  \nHigh-level")
+# number = 23
+# print ("number")
+
+# print('hello kerala \n' * 10)
+# number = [23, 'hfjd', 343, 'ofdo', 13]
+
+# number.append('hai-hello')
+# print(number)
+# number.append('this is a test')
+# print(number)
+
+# number.insert(0, 'hai there')
+# print(number)
+
+# number.remove('hfjd')
+# print(number)
+
+
+# string
+
+
+# name = 'kangaroo'
+
+# print(name[2]) # PRINT THE ELEMENT AT THE GIVEN INDEX.
+# print(name[:3]) # PRINT UP TO THIRD ELEMENT.
+# print(name[0:4]+"oo mon") # we can concatenate a string to the existing string.  
+
+# print[3]='g' # this statement will throw an error because strings are immutable hence they cannot be changed. 
+# print(name)
+
+
+
+# array 
+
+# print("here am experimenting with python in detail about list ")
+
+# number = [123,4335,3543,343,2323,43,34]
+
+
+# print("here am printing a list ")
+# print( number)
+
+# array=['array', 'this','this']
+# print( array)
+
+# print(array[2])
+
+# array[2]= 'this is new array'
+
+# print(array)
+
+# array.append('this is the last item ')
+# print(array)
+
+# array.pop() #removing the last item from the array.
+# print(array)
+
+# array.pop(2) #removing element from the array on given index. 
+# print(array)
+
+# array.insert(3, 'this is the third item ')
+# print(array)
+
+# array.sort()
+# print(array)
+
+# array= ['list of items','d','g','a','b','i']
+# print(array)
+
+# array.sort()
+# print(array) #sorting the array elements properly
+
+# array.extend(['a','b','c','d','new list'])
+# print(array)
+
+# array.remove('a') 
+# print(array)  #only remove the one element of the give value with out removing the its duplicate element 
+
+# array[5]=12  # array is mutiable and so we can change any elements in an array 
+# array[4]=12 
+# array[3]=12 
+# array[2]=12 
+# array[1]=12 
+# print(array) 
+ 
+# print(' now lets start  with lists. in list we use square brackets to arrange items')
+
+# list_items = [11, 12, 13, 14, 15, 16, 17, 18]
+# print(min(list_items))
+# print(max(list_items))
+# print(sum(list_items))
+# list_items.pop() # is used to remove an item from the list.
+# print (list_items)
+# list_items.pop() # pop is a function hence dot't forget the braces 
+# print (list_items)
+# list_items.append('new item found') # to add an item at the end of the list.
+# print(list_items)
+# list_items.insert(0,'item insert at the beginning ') # to insert an item at any given index. 
+# print(list_items)
+# new_list=[2,4,4,5,6,7,8,9,10]
+# list_items.extend([1]) # only list can be extended to another list [] 
+# print(list_items)
+
+# new_list.extend(list_items)
+# print(new_list)
+
+
+# tuples : are list of immutable items that cannot be changed, often uses parenthesis to define tuples 
+
+
+tup = (2, 3, 4, 5, 6, 7, 8, 9, 10)
+print(tup)
+
+print(tup[5]) # to print the fourth item 
+
+#tup[4] = 4 # tuple object doesn't allow value assignment. 
+
+print(tup[2:5]) # to print the selected elements 
+
